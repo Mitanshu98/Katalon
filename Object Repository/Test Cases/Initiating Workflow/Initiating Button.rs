57efbf66610d252787cc9ae9305017283c02b97f
Mitanshu <mitanshugupta98@gmail.com>
2021-05-23 20:51:23 +0530

    Reports are added

---
+++ Object Repository/Test Cases/Initiating Workflow/Initiating Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>This is a button which is used for initiating the workflow</description>
+   <name>Initiating Button</name>
+   <tag></tag>
+   <elementGuidId>44f5a535-97d1-41f3-ad35-658b8c9ce0e3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='B33326379114066709']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Test Cases/Initiating Workflow/Record Revision.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>This button is used for the record revision button where we can see the revision of the record</description>
+   <name>Record Revision</name>
+   <tag></tag>
+   <elementGuidId>fc981061-2593-430a-944d-0d279c6ea1a3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[text()='Record revisions']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
